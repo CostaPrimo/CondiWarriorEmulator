b52de67872f308e29b6dcb2a49c4a88c8243e207
Rickzt <edlund.rikard@gmail.com>
2024-08-28 19:52:12 +0200

    Fixed naming issues and dagger5 calculation

--- CondiWarrior/src/main/Weapons/Dagger.scala
+++ CondiWarrior/src/main/Weapons/Dagger.scala
@@ -6,11 +6,11 @@ class Dagger extends Weapon (1000, 30){
 
   //CONFIG ----------------------------
   private val precise_cut_coeff = 0.6
-  private val precise_cut_coeff_crit = 1.15
+  private val precise_cut_crit_modifier = 1.15
   private val precise_cut_attacks = 1
 
   private val focused_slash_coeff = 0.65
-  private val focused_slash_coeff_crit = 1.15
+  private val focused_slash_crit_modifier = 1.15
   private val focused_slash_attacks = 1
 
   private val keen_strike_coeff = 1.05
@@ -24,25 +24,25 @@ class Dagger extends Weapon (1000, 30){
 
   private val wastrels_ruin_coeff = 1.5
   private val wastrels_ruin_attacks = 1
-  private val wastrels_ruin_coeff_enemynotusingskill = 2.0
+  private val wastrels_ruin_enhanced_coeff = 2.0
 
   private val bladestorm_coeff = 0.5
   private val bladestorm_attacks = 7
 
   private val breaching_strike_coeff = 2.5
   private val breaching_strike_attacks = 1
-  private val breaching_strike_coeff_ifnoenemyboons = 1.5
+  private val breaching_strike_enhanced_coeff = 1.5
 
   private val slicing_maelstrom_coeff = 2.5
   private val slicing_maelstrom_attacks = 1
-  private val slicing_maelstrom_coeff_ifnoenemyboons = 1.5
+  private val slicing_maelstrom_enhanced_coeff = 1.5
 
   //----------------------------------
 
   def precise_cut(cast_time: Double): Unit = {
     tick_time(cast_time)
 
-    TODO: // Needs precise_cut_coeff_crit multiplied if crit
+    TODO: // Needs precise_cut_crit_modifier multiplied if crit
     val damage = new Direct_Hit(this, precise_cut_coeff).hit(precise_cut_attacks)
     getTarget.deal_strike_damage(damage)
   }
@@ -50,7 +50,7 @@ class Dagger extends Weapon (1000, 30){
   def focused_slash(cast_time: Double): Unit = {
     tick_time(cast_time)
 
-    TODO: // Needs focused_slash_coeff_crit multiplied if crit
+    TODO: // Needs focused_slash_crit_modifier multiplied if crit
     val damage = new Direct_Hit(this, focused_slash_coeff).hit(focused_slash_attacks)
     getTarget.deal_strike_damage(damage)
   }
@@ -70,6 +70,7 @@ class Dagger extends Weapon (1000, 30){
 
     if(getPlayer.hasActiveFireField) {
       this.getPlayer.setFireAura(5.0)
+    }
   }
 
   def disrupting_stab(cast_time: Double): Unit = {
@@ -83,22 +84,25 @@ class Dagger extends Weapon (1000, 30){
   def wastrels_ruin(cast_time: Double): Unit = {
     tick_time(cast_time)
 
-    val damage = new Direct_Hit(this, wastrels_ruin_coeff * wastrels_ruin_coeff_enemynotusingskill).hit(wastrels_ruin_attacks)
+    val damage = new Direct_Hit(this, wastrels_ruin_coeff).hit(wastrels_ruin_attacks) * wastrels_ruin_enhanced_coeff
     getTarget.deal_strike_damage(damage)
 
   }
   
-  def bladestorm(cast_time: Double): Unit = {
-    tick_time(cast_time)
-
-    val damage = new Direct_Hit(this, bladestorm_coeff).hit(bladestorm_attacks)
-    getTarget.deal_strike_damage(damage)
+  def bladestorm(duration: Double): Unit = {
+    val weapon_strength = Math.round(this.rollWeaponStrength)
+    for(i <- 1 to bladestorm_attacks){
+      val toTick = duration/ bladestorm_attacks
+      tick_time(toTick)
+      val damage = new Direct_Hit(this, bladestorm_coeff / bladestorm_attacks).single_hit(weapon_strength)
+      getTarget.deal_strike_damage(damage)
+    }
   }
   
   def breaching_strike(cast_time: Double): Unit = {
     tick_time(cast_time)
 
-    val damage = new Direct_Hit(this, breaching_strike_coeff * breaching_strike_coeff_ifnoenemyboons).hit(breaching_strike_attacks)
+    val damage = new Direct_Hit(this, breaching_strike_coeff).hit(breaching_strike_attacks) * breaching_strike_enhanced_coeff
     getTarget.deal_strike_damage(damage)
 
     if(getPlayer.hasActiveFireField) {
@@ -109,7 +113,7 @@ class Dagger extends Weapon (1000, 30){
   def slicing_maelstrom(cast_time: Double): Unit = {
     tick_time(cast_time)
 
-    val damage = new Direct_Hit(this, slicing_maelstrom_coeff * slicing_maelstrom_coeff_ifnoenemyboons).hit(slicing_maelstrom_attacks)
+    val damage = new Direct_Hit(this, slicing_maelstrom_coeff).hit(slicing_maelstrom_attacks) * slicing_maelstrom_enhanced_coeff
     getTarget.deal_strike_damage(damage)
   }
 }
